23fb738b89c4d7350e9f1ed2cff72f4d0ed1a770
Kirill <kirillgovorov@gmail.com>
2023-10-23 23:02:20 +0300

    Task44 is done

--- Task44.py
+++ Task44.py
@@ -1,3 +1,20 @@
+# Задача 44: В ячейке ниже представлен код генерирующий DataFrame,
+# которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
+# Сможете ли вы это сделать без get_dummies?
 
-a = 5
-print(a)
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+
+print(' ')
+
+column_values = data['whoAmI'].unique()
+one_hot_data = pd.DataFrame()
+for i in column_values:
+    one_hot_data[i] = (data['whoAmI'] == i).astype(int)
+print(one_hot_data)
